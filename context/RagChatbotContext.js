dc4467fd0b2f9c43c508df659e691da444b7d0ce
MUNIF <munif@shellbeehaken.com>
2025-06-20 18:06:42 +0600

    update url

--- context/RagChatbotContext.js
+++ context/RagChatbotContext.js
@@ -5,7 +5,8 @@ import "sweetalert2/dist/sweetalert2.min.css";
 
 const MySwal = withReactContent(Swal);
 
-const BASE_URL = "https://f9wf4y0ks6.execute-api.us-east-1.amazonaws.com/prod";
+const BASE_URL =
+  "https://i272k6p9tc.execute-api.us-east-1.amazonaws.com/prod/index";
 
 export const RagChatbotContext = createContext();
 
